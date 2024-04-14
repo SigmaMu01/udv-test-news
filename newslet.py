c6c55efa1d7b3ffa3120e4790afd92930217f953
Mu <mu@test.org>
2024-04-14 05:27:21 +0500

    Added news display for / catalogue feature

--- newslet.py
+++ newslet.py
@@ -4,23 +4,30 @@
 UDV Test News Project
 by Egor Maksimov, 2024
 ---
-*DEV* Вызов API: uvicorn newslet:news_app --reload
+*DEV* Вызов API: uvicorn newslet:news_app
+*DEV* Каталог GET-запросов: http://127.0.0.1:8000/docs
+*DEV* Make sure to check the file for !!! comments
 -
-*PRD* Вызов API: uvicorn newslet:news_app
-*PRD* Адрес страницы: http://127.0.0.1:8000
+*PRD* Адрес главной страницы: http://127.0.0.1:8000
+*PRD* Адрес страницы новостей: http://127.0.0.1:8000/news/{id}
 """
 
 from fastapi import FastAPI
-import json
 import sys
+from newslet_json_parser import *     # Функции, возвращающие запросы новостей и комментариев:
+                                        #news_records, 
 
-
-if sys.version_info[0] < 3 and sys.version_info[0] < 10:    # Проверка версии
-    raise Exception("Python 3.10 or a more recent version is required.")
+if sys.version_info[0] < 3 and sys.version_info[0] < 12:    # Проверка версии Python
+    raise Exception("Python 3.12 or a more recent version is required.")    # !!! Check if Python 3.10 runs it okay
 
 news_app = FastAPI()    # Вызов API
 
 
 @news_app.get("/")
 async def root():
-    return {"message": "Hello, News!"}
+    return {"news": news_records()}
+
+
+#@news_app.get("/news/{id}")
+#async def read_news():
+#    return {"id": "Hello, "}
---
+++ newslet_json_parser.py
@@ -0,0 +1,50 @@
+"""
+UDV Test News Project
+Functions library
+by Egor Maksimov, 2024
+---
+*DEV* Make sure to check the file for !!! comments
+"""
+
+import json
+
+
+f_news = open('news.json', encoding="utf-8")
+f_comments = open('comments.json', encoding="utf-8")
+
+data_news = json.load(f_news)
+data_comments = json.load(f_comments)
+
+
+def comments_count():
+
+    c_count = {}        # Возвращаем словарь {id новости: кол-во комментов}, чтобы проверка занимала O(n)
+    gen = (data_entry for data_entry in data_comments['comments'])
+    
+    for c_entry in gen:
+        id = c_entry['news_id']
+        if id not in c_count.keys():
+            c_count[id] = 1
+        else:
+            c_count[id] += 1
+            
+    return c_count
+
+
+def news_records():     # Выгрузка всех новостей одним запросом
+
+    res = []
+    c_count = comments_count()  # Cловарь {id новости: кол-во комментов}
+    
+    gen = (data_entry for data_entry in data_news['news'])
+    
+    for n_entry in gen:
+        count = c_count[n_entry['id']] if n_entry['id'] in c_count else 0   #Если комментариев нет, то 0
+        n_entry['comments_count'] = count
+        res.append(n_entry)
+    
+    return res
+
+
+f_news.close()
+f_comments.close()
