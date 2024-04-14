9c8b67156476d138139442e5b6e5e639e398434d
Mu <mu@test.org>
2024-04-14 18:04:18 +0500

    Added 404 parser

--- newslet.py
+++ newslet.py
@@ -12,7 +12,7 @@ by Egor Maksimov, 2024
 *PRD* Адрес страницы новостей: http://127.0.0.1:8000/news/{id}
 """
 
-from fastapi import FastAPI
+from fastapi import FastAPI, HTTPException
 import sys
 from newslet_json_parser import *     # Функции, возвращающие запросы новостей и комментариев:
                                         #news_records, 
@@ -25,10 +25,18 @@ news_app = FastAPI()    # Вызов API
 
 @news_app.get("/")
 async def root():
+
     res = news_records()
     return {"news": res, "news_count": len(res)}
 
 
-#@news_app.get("/news/{id}")
-#async def read_news():
-#    return {"id": "Hello, "}
+@news_app.get("/news/{item_id}")
+async def read_news(item_id: int):
+
+    news = {1: 'yay', 2: 'nay', 3: 'deleted'}
+    if item_id not in news:
+        raise HTTPException(status_code=404, detail="News not found")
+    elif news[item_id] == 'deleted':
+        raise HTTPException(status_code=404, detail="News deleted")
+        
+    return {"item": news[item_id]}
